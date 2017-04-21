1390ec2fd9cafb55f2673f402b4fe61b16e45a8e
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-21 12:48:47 -0400

    added .gitignore

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "Bamazon"
+});
+
+connection.connect(function(err, res) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
