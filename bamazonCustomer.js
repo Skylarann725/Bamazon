ac4ef5b282c48190500dd3426014813b98c5bb05
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-23 16:05:18 -0400

    added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,6 +25,7 @@ connection.connect(function(err, res) {
 connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
 
+    // to create the table in the terminal
     var table = new Table({
         head: ["Item ID: ", "Product Name: ", "Price: ", "Department: ", "In Stock: "],
         chars: {
@@ -45,17 +46,21 @@ connection.query("SELECT * FROM products", function(err, res) {
             'middle': '│'
         }
     });
+    // for loop to display all of the products
     for (i = 0; i < res.length; i++) {
         table.push(
             [res[i].item_id, res[i].product_name, res[i].price, res[i].department_name, res[i].stock_quantity]
         );
     }
+    // to console log the table
     console.log(table.toString());
 
+    // calls the customerPurchase function
     customerPurchase();
 });
 
 var customerPurchase = function() {
+  // questions for inquire prompt
     var questions = [{
         type: 'list',
         name: 'Item_Id',
@@ -70,18 +75,23 @@ var customerPurchase = function() {
     inquirer.prompt(questions).then(function(answers) {
         connection.query("SELECT * FROM `products` WHERE `item_id`=?", [answers.Item_Id], function(err, res) {
             if (err) throw err;
+            // converts answers.Quantity from a string into an integer
             answers.Quantity = parseInt(answers.Quantity);
+            // if statement to see if we have a certain product in stock
             if (res[0].stock_quantity < answers.Quantity) {
                 console.log('I am sorry. We are currently out of that item. Please check back soon.');
                 customerPurchase();
             } else {
+                // new stock quantity variable = current stock quantity minus how many items the customer took
                 var newQuantity = res[0].stock_quantity - answers.Quantity;
+                // query to update the table with the new stock quantity
                 connection.query("UPDATE `products` SET ? WHERE ?", [{
                     stock_quantity: newQuantity
                 }, {
                     item_id: answers.Item_Id
                 }]);
 
+                // query to show the new table with the updated stock quantities
                 connection.query("SELECT * FROM products", function(err, res) {
                         var table = new Table({
                             head: ["Item ID: ", "Product Name: ", "Price: ", "Department: ", "In Stock: "],
@@ -110,8 +120,9 @@ var customerPurchase = function() {
                         }
                         console.log(table.toString());
                     }) // end of query function
-
+                // total for the client = price times client's quantity amount
                 var total = res[0].price * answers.Quantity;
+                // converts total to an integer
                 var customerTotal = parseInt(total);
                 console.log("Your total is: $" + customerTotal);
                 console.log("Thank you! Come again soon!")
